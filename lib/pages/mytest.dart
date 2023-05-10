432f1dc54f0ec4067da2a30bdbba775f26f2846d
Bronziel Gold <morgangiermogrimes@gmail.com>
2023-05-10 18:17:22 +0200

    widget open with one svg

---
+++ lib/pages/mytest.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class testwidget extends StatefulWidget {
+  const testwidget({Key? key}) : super(key: key);
+
+  @override
+  _testwidget createState() => _testwidget();
+}
+
+class _testwidget extends State<testwidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Recepie List')),
+        body: SingleChildScrollView(
+            child: Container(
+                child: SvgPicture.asset(
+          'Images/texas.svg',
+        )
+
+                // Add your widget tree here
+                )));
+  }
+}
