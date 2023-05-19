0565184fa1d958cc571d1f9d133924f3370efbb5
Bronziel Gold <morgangiermogrimes@gmail.com>
2023-05-19 18:24:00 +0200

    tt

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,10 @@ import 'pages/gtpforms/add_recipe_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(App());
 }
 
